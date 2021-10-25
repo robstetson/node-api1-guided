ba185aa252fe1a67dadfc2c5b3f9201cbe6964cc
Ladrillo <gabriel@cabrejas.net>
2021-10-25 12:49:43 -0400

    wip

--- api/server.js
+++ api/server.js
@@ -1,5 +1,6 @@
 // IMPORTS AT THE TOP
 const express = require('express')
+const Dog = require('./dog-model')
 // INSTANCE OF EXPRESS APP
 const server = express()
 // GLOBAL MIDDLEWARE
@@ -11,6 +12,7 @@ server.get('/', (req, res) => {
   res.json({ message: 'hello, world'})
 })
 // [GET] /api/dogs (R of CRUD, fetch all dogs)
+server.get('/api/dogs')
 // [GET] /api/dogs/:id (R of CRUD, fetch dog by :id)
 // [POST] /api/dogs (C of CRUD, create new dog from JSON payload)
 // [PUT] /api/dogs/:id (U of CRUD, update dog with :id using JSON payload)
